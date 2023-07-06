cda2cf3cc2147c3093930bad339d848f9281782f
tkachuk <serhii.tkachuk@visiquate.com>
2023-07-06 14:11:53 -0500

    #4 fixed newsApp.jsx

--- src/containers/newsApp.jsx
+++ src/containers/newsApp.jsx
@@ -1,85 +1,68 @@
-﻿import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+﻿import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import NewsItem from '../components/newsItem.jsx'
+import { fetchUsers, showMore } from '../actions/newsActions'
 
-import * as actions from '../actions/newsActions.js'
+const NewsApp = () => {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.news.data)
+  const showBlockVal = useSelector((state) => state.news.showBlockVal)
 
-class NewsApp extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componenDidMount() {
-    if (this.props.data.length === 0) {
-      this.props.fetchUsers('data/data.json')
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(fetchUsers('assets/data/data.json'))
     }
-  }
+  }, [])
 
-  render() {
-    let monthNames = [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
-    ]
+  let monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
 
-    let showCount = this.props.showBlockVal * 8
+  let showCount = showBlockVal * 8
 
-    let items = this.props.data.slice(0, showCount).map((item) => {
-      let date = new Date(item.date)
-      let dateStr = `${date.getDate()} ${monthNames[date.getMonth()]}`
-      let props = {
-        key: item.id,
-        id: item.id,
-        title: `Amazing Post #${item.id}`,
-        date: dateStr,
-        src: 'assets/img/news-img1.jpeg',
-      }
-
-      return <NewsItem {...props} />
-    })
+  let items = data.slice(0, showCount).map((item) => {
+    let date = new Date(item.date)
+    let dateStr = `${date.getDate()} ${monthNames[date.getMonth()]}`
+    let props = {
+      key: item.id,
+      id: item.id,
+      title: `Amazing Post #${item.id}`,
+      date: dateStr,
+      src: 'assets/img/news-img1.jpeg',
+    }
 
     return (
-      <div>
-        <div className='row'>{items}</div>
-        <div className='news-btn-container'>
-          <span
-            className='btn news-load-btn'
-            onClick={() => this.props.showMore()}
-          >
-            <i className='fa fa-plus fa-1x' />
-            Load more
-          </span>
-        </div>
-      </div>
+      <React.Fragment key={item.id}>
+        <NewsItem {...props}></NewsItem>
+      </React.Fragment>
     )
-  }
-}
+  })
 
-function mapStateToProps(state) {
-  return {
-    data: state.news.data,
-    showBlockVal: state.news.showBlockVal,
-  }
-}
-
-function matchDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      fetchUsers: actions.fetchUsers,
-      showMore: actions.showMore,
-    },
-    dispatch
+  return (
+    <div>
+      <div className='row'>{items}</div>
+      <div className='news-btn-container'>
+        <span
+          className='btn news-load-btn'
+          onClick={() => dispatch(showMore())}
+        >
+          <i className='fa fa-plus fa-1x' />
+          Load more
+        </span>
+      </div>
+    </div>
   )
 }
 
-export default connect(mapStateToProps, matchDispatchToProps)(NewsApp)
+export default NewsApp
