d9f2615cc2720e93f67173e3a381d4c9b2eb5f5d
tkachuk <serhii.tkachuk@visiquate.com>
2023-07-06 12:32:35 -0500

    #4 fixed feedbackApp.jsx

--- src/containers/feedbackApp.jsx
+++ src/containers/feedbackApp.jsx
@@ -1,20 +1,19 @@
-﻿import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+﻿import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Cookies from 'universal-cookie'
-
 import FeedbackItem from '../components/feedbackItem.jsx'
-import * as actions from '../actions/feedbackActions.js'
+import {
+  addComment,
+  removeComment,
+  initComments,
+} from '../actions/feedbackActions'
 
-class FeedbackApp extends React.Component {
-  constructor(props) {
-    super(props)
-    this.cookies = new Cookies()
-    this.addCommentHandler = this.addCommentHandler.bind(this)
-    this.removeCommentHandler = this.removeCommentHandler.bind(this)
-  }
+const FeedbackApp = () => {
+  const dispatch = useDispatch()
+  const messages = useSelector((state) => state.feedback.messages)
+  const cookies = new Cookies()
 
-  addCommentHandler() {
+  const addCommentHandler = () => {
     let title = document.getElementsByName('feedback-title')[0].value
     let message = document.getElementsByName('feedback-message')[0].value
     if (title === '' || message === '') {
@@ -22,81 +21,57 @@ class FeedbackApp extends React.Component {
       return
     }
 
-    this.props.addComment({ title, message })
+    dispatch(addComment({ title, message }))
 
     document.getElementsByName('feedback-title')[0].value = ''
     document.getElementsByName('feedback-message')[0].value = ''
   }
 
-  removeCommentHandler(e) {
-    this.props.removeComment(e.target.dataset.id)
+  const removeCommentHandler = (e) => {
+    dispatch(removeComment(e.target.dataset.id))
   }
 
-  componentWillMount() {
-    let data = this.cookies.get('messages')
+  useEffect(() => {
+    let data = cookies.get('messages')
     if (data === undefined) {
       data = []
     }
 
-    this.props.initComments(data)
-  }
+    dispatch(initComments(data))
+  }, [])
 
-  componentDidUpdate() {
-    this.cookies.set('messages', this.props.messages, { path: '/' })
-  }
+  useEffect(() => {
+    cookies.set('messages', messages, { path: '/' })
+  }, [messages])
 
-  render() {
-    let items = this.props.messages.map((item, index) => {
-      return (
-        <FeedbackItem
-          key={index}
-          id={item.id}
-          title={item.title}
-          message={item.text}
-          remove={this.removeCommentHandler}
-        />
-      )
-    })
+  const items = messages.map((item, index) => (
+    <FeedbackItem
+      key={index}
+      id={item.id}
+      title={item.title}
+      message={item.text}
+      remove={removeCommentHandler}
+    />
+  ))
 
-    return (
-      <div>
-        <div className='feedback-container'>{items}</div>
-        <form className='row feedback-add-form'>
-          <div className='col-xs-12 col-sm-8 col-md-offset-1 col-md-6'>
-            <input type='text' name='feedback-title' placeholder='Title' />
-            <textarea name='feedback-message' placeholder='Message' />
-          </div>
-          <div className='col-xs-12 col-sm-4 feedback-btn-container'>
-            <h1>Leave us a message</h1>
-            <span
-              className='btn feedback-add-btn'
-              onClick={this.addCommentHandler}
-            >
-              <i className='fa fa-plus fa-1x' />
-              Add comment
-            </span>
-          </div>
-        </form>
-      </div>
-    )
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    messages: state.feedback.messages,
-  }
-}
-
-function matchDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      addComment: actions.addComment,
-      removeComment: actions.removeComment,
-      initComments: actions.initComments,
-    },
-    dispatch
+  return (
+    <div>
+      <div className='feedback-container'>{items}</div>
+      <form className='row feedback-add-form'>
+        <div className='col-xs-12 col-sm-8 col-md-offset-1 col-md-6'>
+          <input type='text' name='feedback-title' placeholder='Title' />
+          <textarea name='feedback-message' placeholder='Message' />
+        </div>
+        <div className='col-xs-12 col-sm-4 feedback-btn-container'>
+          <h1>Leave us a message</h1>
+          <span className='btn feedback-add-btn' onClick={addCommentHandler}>
+            <i className='fa fa-plus fa-1x' />
+            Add comment
+          </span>
+        </div>
+      </form>
+    </div>
   )
 }
 
-export default connect(mapStateToProps, matchDispatchToProps)(FeedbackApp)
+export default FeedbackApp
